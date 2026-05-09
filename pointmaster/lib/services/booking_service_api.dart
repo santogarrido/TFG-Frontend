fe0aec14ad884307a3b36aed90a46cc6e7901210
santogarrido <santocildesgarridoraul@gmail.com>
2026-05-09 12:06:32 +0200

    Added services

---
+++ pointmaster/lib/services/booking_service_api.dart
@@ -0,0 +1,140 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:pointmaster/models/response_api.dart';
+
+class BookingServiceApi {
+  static const String _baseUrl = 'https://sportaqs-backend.onrender.com';
+
+  //Get all
+  Future<ResponseApi> getAllBookings(String token) async {
+    Uri url = Uri.parse('$_baseUrl/bookings');
+
+    final response = await http.get(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    return ResponseApi.fromJson(json.decode(response.body));
+  }
+
+  // Get all bookings of a Court
+  Future<ResponseApi> getBookingsByCourt(int courtId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/bookings/getByCourt/$courtId');
+
+    final response = await http.get(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    return ResponseApi.fromJson(json.decode(response.body));
+  }
+
+  // Get all bookings of a Facility
+  Future<ResponseApi> getBookingsByFacility(
+    int facilityId,
+    String token,
+  ) async {
+    Uri url = Uri.parse('$_baseUrl/bookings/getAllBookings/$facilityId');
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    final responseApi = ResponseApi.fromJson(json.decode(response.body));
+    return responseApi;
+  }
+
+  // Get bookings of an User
+  Future<ResponseApi> getBookingsByUser(int userId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/bookings/getBookingsByUser/$userId');
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    final responseApi = ResponseApi.fromJson(json.decode(response.body));
+    return responseApi;
+  }
+
+  // Get booking by id
+  Future<ResponseApi> getBookingById(int bookingId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/bookings/getBooking/$bookingId');
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    final responseApi = ResponseApi.fromJson(json.decode(response.body));
+    return responseApi;
+  }
+
+  // Crear una nueva reserva
+  Future<ResponseApi> addBooking(
+    int userId,
+    int courtId,
+    DateTime bookingDateTime,
+    DateTime courtDateTimeBooking,
+    String token,
+  ) async {
+    Uri url = Uri.parse('$_baseUrl/bookings/addBooking');
+
+    final response = await http.post(
+      url,
+      body: jsonEncode({
+        'userId': userId,
+        'courtId': courtId,
+        'bookingDateTime': bookingDateTime.toIso8601String(),
+        'courtDateTimeBooking': courtDateTimeBooking.toIso8601String(),
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    final responseApi = ResponseApi.fromJson(json.decode(response.body));
+    return responseApi;
+  }
+
+  // Delete booking
+  Future<ResponseApi> deleteBooking(int bookingId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/bookings/deleteBooking/$bookingId');
+
+    final response = await http.delete(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    final responseApi = ResponseApi.fromJson(json.decode(response.body));
+    return responseApi;
+  }
+}
---
+++ pointmaster/lib/services/facility_service.dart
@@ -0,0 +1,112 @@
+import "dart:convert";
+
+import "package:http/http.dart" as http;
+import "package:pointmaster/models/response_api.dart";
+
+class FacilityService {
+
+  //static const String _baseUrl = 'http://localhost:8090'; 
+  static const String _baseUrl = 'https://sportaqs-backend.onrender.com/facilities';
+
+  Future<ResponseApi> getFacilities(String token) async {
+
+    Uri url = Uri.parse('$_baseUrl/getAll');
+
+    final response = await http.get(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = ResponseApi.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+  }
+
+  Future<ResponseApi> getFacilityById(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/getFacility/$id');
+
+    final response = await http.get(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = ResponseApi.fromJson(json.decode(response.body));
+
+    return facilityResponse; 
+
+  }
+
+  Future<ResponseApi> addFacility(String name, String openTime, String closeTime, String location, String token) async {
+    Uri url = Uri.parse('$_baseUrl/addFacility');
+
+    final response = await http.post(
+      url,
+      body: json.encode({'name' : name, 'openTime' : openTime, 'closeTime' : closeTime, 'location' : location}),
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = ResponseApi.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }  
+
+  Future<ResponseApi> deleteFacility(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/deleteFacility/$id');
+
+    final response = await http.delete(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = ResponseApi.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }
+
+  Future<ResponseApi> activateFacility(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/activateFacility/$id');
+
+    final response = await http.post(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = ResponseApi.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }
+
+  Future<ResponseApi> deactivateFacility(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/deactivateFacility/$id');
+
+    final response = await http.post(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = ResponseApi.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }
+
+  Future<ResponseApi> updateFacility(int id, String name, String openTime, String closeTime, String location, String token) async {
+    Uri url = Uri.parse('$_baseUrl/updateFacility/$id');
+
+    final response = await http.put(
+      url,
+      body: json.encode({'name' : name, 'openTime' : openTime, 'closeTime' : closeTime, 'location' : location}),
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    
+    final facilityResponse = ResponseApi.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+    
+  }
+
+}
